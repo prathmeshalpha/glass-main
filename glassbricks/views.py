34852817513bfd367df48483d007835a6eecd038
Kartik Jain <kartikvyomfortp@gmail.com>
2024-11-16 02:44:28 +0530

    Update views.py

--- glassbricks/views.py
+++ glassbricks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.mail import send_mail, EmailMessage
 from django.template.loader import render_to_string
