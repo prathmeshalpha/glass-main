1b11ac10325611ba3e45ca999cd960e987d1b6b3
prathmeshalpha <prathmesh@alphamotion.in>
2024-11-16 15:29:41 +0530

    Update settings.py

--- property/settings.py
+++ property/settings.py
@@ -104,7 +104,8 @@ DATABASES = {
     }
 }
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
